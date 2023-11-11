56b6f5f93a4771c042adfe73511743f61060341a
nitintf <nitinpanwar0802@gmail.com>
2023-11-12 04:35:57 +0530

    removing sanity studio page

--- app/(studio)/admin/Studio.tsx
+++
@@ -1,8 +0,0 @@
-'use client'
-import { NextStudio } from 'next-sanity/studio'
-
-import config from '@/lib/sanity/sanity.config'
-
-export function Studio() {
-	return <NextStudio config={config} />
-}
