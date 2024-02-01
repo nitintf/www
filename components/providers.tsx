1767ef4894227195ae78224f6f8f5ce41d56891c
nitintf <nitinpanwar0802@gmail.com>
2024-02-01 11:42:11 +0530

    refactor: removing unused lenis hook for smooth scrolling

--- components/providers.tsx
+++ components/providers.tsx
@@ -2,11 +2,9 @@
 
 import { ThemeProvider } from 'next-themes'
 import { PropsWithChildren } from 'react'
-import { ReactLenis, useLenis } from '@studio-freight/react-lenis'
+import { ReactLenis } from '@studio-freight/react-lenis'
 
 export const Providers: React.FC<PropsWithChildren> = ({ children }) => {
-	useLenis()
-
 	return (
 		<ReactLenis root>
 			<ThemeProvider defaultTheme='dark'>{children}</ThemeProvider>
